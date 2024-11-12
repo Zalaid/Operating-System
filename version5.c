e6ef9ae8ae72163af13b3ca70d546f470ba58e21
Zalaid <zalaidbutt1@gmail.com>
2024-11-12 22:50:18 +0500

    version-5- Function to display the shell prompt

--- version5.c
+++ version5.c
@@ -9,3 +9,19 @@
 #define BUFFER_SIZE 1024
 #define HISTORY_SIZE 10
 #define MAX_BG_PROCESSES 100
+
+char* history[HISTORY_SIZE];
+int history_count = 0;
+
+pid_t bg_processes[MAX_BG_PROCESSES];
+int bg_process_count = 0;
+
+// Function to display the shell prompt with the current working directory
+void display_prompt() {
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("PUCITshell@%s:- ", cwd);
+    } else {
+        perror("getcwd() error");
+    }
+}
